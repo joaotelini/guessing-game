f90465dfa0d2547bedf145fb0cc9ff1e5c9c945a
TeLiNi <joaotelini03@gmail.com>
2024-10-02 23:44:10 -0300

    better code readability

--- app.js
+++ app.js
@@ -4,69 +4,72 @@ const dicas = document.getElementById("dicas");
 
 const tentativasHTML = document.getElementById("tentativas");
 
-const status = document.getElementById("status");
+const status = document.getElementById("status")
 
-const tentativasSalva = document.getElementById("tentativasSalva");
+const tentativasSalva = document.getElementById("tentativasSalva")
 
-const pNumeroTentados = document.getElementById("pNumeroTentados");
+const pNumeroTentados = document.getElementById("pNumeroTentados")
 
-let numeroSecreto = Math.floor(Math.random() * 1001);
+let numeroSecreto = Math.floor(Math.random() * 1001)
 
-let li = document.createElement("li");
+let li = document.createElement("li")
 
-console.log(numeroSecreto);
+// console.log(numeroSecreto)
 
-let tentativas = 0;
+let tentativas = 0
 
-let tentativasArray = [];
+let tentativasArray = []
 
 function getInput() {
-  const valorInput = parseInt(input.value);
+  const valorInput = parseInt(input.value)
 
   if (isNaN(valorInput)) {
-    return console.error("Erro");
+    return console.error("Erro")
   }
-  // console.log("Você digitou: " + valorInput);
-  tentativas += 1;
-  verificarAcerto(valorInput);
-  limparInput();
+
+  tentativas += 1
+  verificarAcerto(valorInput)
+  limparInput()
 }
 
 function limparInput() {
-  input.value = "";
+  input.value = ""
 }
 
 function limparText() {
-  dicas.innerHTML = "";
-  tentativasHTML.innerHTML = "";
-  status.innerHTML = "";
+  dicas.innerHTML = ""
+  tentativasHTML.innerHTML = ""
+  status.innerHTML = ""
 }
 
 function resetGame() {
-  numeroSecreto = Math.floor(Math.random() * 1001);
+  numeroSecreto = Math.floor(Math.random() * 1001)
   tentativas = 0
   tentativasArray = []
   limparInput()
   limparText()
   pNumeroTentados.innerHTML = ""
   li.innerHTML = ""
-  console.log(numeroSecreto);
+  // console.log(numeroSecreto)
 }
 
 function verificarAcerto(valorInput) {
-  tentativasArray.push(valorInput);
+  tentativasArray.push(valorInput)
+
   if (valorInput === numeroSecreto) {
-    limparText();
-    status.innerHTML =
-      "Parabens você acertou! Numero de tentativas: " + tentativas;
-    pNumeroTentados.innerHTML = "Números tentados: ";
-    li.innerHTML = tentativasArray.join(", ");
-    tentativasSalva.appendChild(li);
-  } else {
+    limparText()
+    status.innerHTML = "Parabens você acertou! Número de tentativas: " + tentativas
+    pNumeroTentados.innerHTML = "Números tentados: "
+    li.innerHTML = tentativasArray.join(", ")
+    tentativasSalva.appendChild(li)
+  } 
+  else {
     if (valorInput > numeroSecreto) {
-      dicas.innerHTML = "O número digitado é maior que o número secreto ⬆";
-    } else {
-      dicas.innerHTML = "O número digitado é menor que o número secreto ⬇";
+      dicas.innerHTML = "O número digitado é maior que o número secreto ⬆"
+    } 
+    
+    else {
+      dicas.innerHTML = "O número digitado é menor que o número secreto ⬇"
     }
   }
 }
